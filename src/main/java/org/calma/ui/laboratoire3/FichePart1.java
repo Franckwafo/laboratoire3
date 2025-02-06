651b2ef148699a5f13077fb471eb7e793fb87a38
202330093 <202330093@collegealma.ca>
2025-02-06 09:13:28 -0500

    Premier commit

---
+++ src/main/java/org/calma/ui/laboratoire3/FichePart1.java
@@ -0,0 +1,16 @@
+package org.calma.ui.laboratoire3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FichePart1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
